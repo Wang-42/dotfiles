b54e6704277b2766624fe11046db4615f88c83c7
Wang <trananhsangmattroi@gmail.com>
2026-03-24 19:44:59 +0700

    first commit

---
+++ fish/config.fish
@@ -0,0 +1,15 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+function fish_greeting
+    #    # smth smth
+end
+
+alias gcam="git commit -a -m"
+alias gcm="git commit -m"
+alias wgup="sudo wg-quick up"
+alias wgdown="sudo wg-quick down"
+alias mr="wl-mirror eDP-1"
+alias fafe="clear && fastfetch"
+alias clr="clear"
